e54f82a492e1511f0770b4b26ee7d03d12e4354a
Dmitry Mezhinsky <mezhinsky.dmitry@gmail.com>
2026-01-18 21:21:52 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,7 @@ import {
 } from "react";
 import { createRoot } from "react-dom/client";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -58,7 +54,7 @@ const withSuspense = (Component: LazyExoticComponent<ComponentType>) => (
 );
 
 /**
- * Auth wrapper component that provides AuthProvider with router context
+ * Auth wrapper component that provides AuthProvider with router context and
  */
 function AuthWrapper() {
   return (
@@ -138,5 +134,5 @@ createRoot(document.getElementById("root")!).render(
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
     <Toaster />
-  </StrictMode>
+  </StrictMode>,
 );
